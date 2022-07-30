fc317d4cdbce58647a77c7bfc2300332a0741113
pol-murakami <96195951+hiro1103@users.noreply.github.com>
2022-07-31 06:41:28 +0900

    murakami

---
+++ chap04/foreach.py
@@ -0,0 +1,9 @@
+data = ["うめ", "さくら", "もも"]
+
+for value in data:
+    print(value)
+
+print()
+
+for i in range(1, 6):
+    print(i, "番目のループ")
---
+++ chap04/page125.py
@@ -0,0 +1,10 @@
+tensu = 75
+
+if tensu >= 90:
+    print("優")
+elif tensu >= 70 and tensu < 90:
+    print("良")
+elif tensu >= 50 and tensu < 70:
+    print("可")
+else:
+    print("50点未満")
